2b599da2e89a6f6c71512317f22e43068b9aaf01
Dominic Dale <dominicdale@googlemail.com>
2017-02-15 22:57:58 +0000

    Added overlay functionality

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,9 +1,15 @@
 $(function(){
   figure = $('.gallery figure');
   wrapper = $('.wrapper');
+  overlay = $('.overlay');
 
   figure.click(function(){
     wrapper.toggleClass("shrink");
-    $(this).toggleClass("active");
+    overlay.toggleClass("slide");
+  })
+
+  $('.close').click(function(){
+    overlay.toggleClass("slide");
+    wrapper.toggleClass("shrink");
   })
 })
