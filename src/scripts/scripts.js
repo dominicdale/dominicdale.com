e4a1a2d197dd9844d0987cb314b92e595897c10f
Dominic Dale <dominicdale@googlemail.com>
2017-04-26 19:19:49 +0100

    More work on overlay functionality

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,25 +1,57 @@
 $(function(){
+
+  // variables
+
   figure = $('.gallery figure');
   wrapper = $('.wrapper');
   overlay = $('.overlay');
+  overlayContent = $('.overlay-content');
+  overlayLoading = $('.overlay-loading');
+
+
+  // Click on gallery item
 
   figure.click(function(){
-    wrapper.toggleClass("shrink");
-    overlay.toggleClass("slide");
-    overlay.css('background-color', $(this).data("color"));
+    $('body').toggleClass('no-scroll');
+    wrapper.toggleClass('shrink');
+    overlay.toggleClass('slide');
+    overlay.css('background-color', $(this).data('color'));
     $('.close').toggleClass('active');
-    $('.overlay-loading').toggleClass('active');
-    $('.overlay-content').load($(this).data("url"), function(){
-      $('.overlay-loading').removeClass('active');
+    overlayLoading.toggleClass('active');
+    overlayContent.load($(this).data('url'), function(){
+      setTimeout(function(){
+        overlayLoading.removeClass('active');
+        overlayContent.addClass('active');
+      }, 1500)
     });
   })
 
 
+  // Close gallery overlay
+
   $('.close').click(function(){
-    $(this).removeClass('active');
-    overlay.toggleClass("slide");
-    wrapper.toggleClass("shrink");
-    $('.overlay-loading').removeClass('active');
+    $('body').toggleClass('no-scroll');
+    overlay.toggleClass('slide');
+    wrapper.toggleClass('shrink');
+    overlayLoading.removeClass('active');
+    overlayContent.removeClass('active');
   })
+
+
+  // smooth scrolling
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        return false;
+      }
+    }
+  });
+
 
 })
